e183c92671776d90427f59b015d518dd8f729089
Nem-Ajvaz <45509371+Nem-Ajvaz@users.noreply.github.com>
2021-07-05 22:32:25 +1000

    Uploading work

---
+++ README.md
@@ -0,0 +1,43 @@
+# homework_3
+
+## Installation
+
+To clone and run this project, please make sure you have git installed.
+
+run `git clone https://github.com/Nem-Ajvaz/homework_3.git`
+
+## Introduction
+
+The purpose of this project was to build a password generator which met the following conditions:
+
+Length between 8 - 128
+Conditional lowercase letters
+Conditional uppercase letters
+Conditional numbers
+Conditional special characters
+
+## Images
+
+Input
+
+![Number less than 8](/assets/images/screenshots/Less-than-8.png)
+![Greater than 128](/assets/images/screenshots/More-than-128.png)
+![Please choose between 8 and 128](/assets/images/screenshots/Enter-correct-value.png)
+
+Include
+
+![Include Numbers](/assets/images/screenshots/Include-numbers.png)
+![Include Special Characters](/assets/images/screenshots/Include-special-chars.png)
+![Include Lower Case](/assets/images/screenshots/Include-lowercase.png)
+![Include Upper Case](/assets/images/screenshots/Include-uppercase.png)
+
+Password Generation
+![Generated password](assets/images/screenshots/Generated-password.png)
+
+## Issues faced
+
+The major issue faced was with the correct logic for assigning random indexes of the string to the generated password.
+
+## Lisence
+
+MIT
---
+++ asset/js/script.js
@@ -0,0 +1,69 @@
+const $contentEle = $("#mainSec");
+const $clockDisplay = $("#clockDisplay");
+
+const dayStart = 9;
+const dayEnd = 24;
+
+function renderTime() {
+  const momentDate = moment();
+
+  $clockDisplay.text(momentDate.format("dddd Do MMMM YYYY -- hh:mm:ss a"));
+  setTimeout(renderTime, 1000);
+}
+
+const LOCAL_STORAGE_PREPEND_KEY = "dayPlanner:";
+
+// Retrieving Todo from local storage
+function getTodoFromLocalStorage(key) {
+  return localStorage.getItem(key);
+}
+
+// Storing Todo in local storage
+function setTodoFromLocalStorage(key, value) {
+  localStorage.setItem(LOCAL_STORAGE_PREPEND_KEY + key, value);
+}
+
+function renderTable() {
+  for (let i = dayStart; i <= dayEnd; i++) {
+    const momentNow = moment();
+    const momentTimeStamp = moment().set("hour", i);
+    const savedValue = getTodoFromLocalStorage(LOCAL_STORAGE_PREPEND_KEY + i);
+
+    // Creating elements
+    const $rowEle = $("<div>");
+    const $timeEle = $("<p>");
+    const $todoEle = $("<input>");
+    const $btnEle = $("<button>");
+    // Adding classes
+    $rowEle.addClass("row");
+    $timeEle.addClass("sched-time");
+    $todoEle.addClass("todo");
+    $todoEle.val(savedValue);
+    $btnEle.addClass("save");
+    // Appending
+    $contentEle.append($rowEle);
+    $rowEle.append($timeEle);
+    $rowEle.append($todoEle);
+    $rowEle.append($btnEle);
+    // Assigning time to the todoEle
+    $timeEle.text(momentTimeStamp.format("h a"));
+    $btnEle.text("Save");
+    // Assigning classes based on condition
+    if (momentTimeStamp.isBefore(momentNow, "hour")) {
+      $timeEle.addClass("past");
+      $todoEle.addClass("past");
+    } else if (momentTimeStamp.isAfter(momentNow, "hour")) {
+      $timeEle.addClass("future");
+      $todoEle.addClass("future");
+    } else {
+      $timeEle.addClass("present");
+      $todoEle.addClass("present");
+    }
+    $btnEle.on("click", function () {
+      setTodoFromLocalStorage(i, $todoEle.val());
+    });
+  }
+}
+
+renderTable();
+document.body.onload = renderTime;
